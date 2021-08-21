f7a20d43985e33c2d4bd3779a071b602ae55b692
PranaKnight <80462531+PranaKnight@users.noreply.github.com>
2021-08-21 04:29:05 -0300

    Add files via upload
    Cripto Cesar

--- ransom.py
+++ ransom.py
@@ -1,14 +1,31 @@
 import string
 import sys
-file = open('arquivo.txt' , 'r')
+file = open('D:\Python\RSA\Teste.txt' , 'r', encoding='utf-8')
 rotation = int(input("Input the key: "))
-message = str(file.read().lower())
-alfabet = list(string.ascii_lowercase)
+message = str(file.readline().lower())
+print(message)
+alfabet = list(string.printable)
 result = ''
+
 for character in message:
     if character in alfabet:
         position = alfabet.index(character)
-        position = (position - rotation) % 26
+        position = (position - rotation) % 100
         result = result + alfabet[position]
     print (result)
 file.close()
+
+cript = result
+
+arquivo = open('D:\Python\RSA\cripto.txt', 'w', encoding='utf-8')
+arquivo.writelines(result)
+arquivo.close
+
+decript = ''
+for character in cript:
+    if character in alfabet:
+        position = alfabet.index(character)
+        position = (position + rotation) % 100
+        decript = decript + alfabet[position]
+    print (decript)
+    
