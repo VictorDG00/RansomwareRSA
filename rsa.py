3d37f52bfb18d757bca5a0dc317338734d7ec6a1
victordg00 <victord.goncalves@outlook.com>
2021-09-10 23:57:25 -0300

    1° step numeros primos e totiente

--- rsa.py
+++ rsa.py
@@ -1,14 +1,32 @@
-import random
-# criar 2 numero primos
+'''
+FIAP
+Defesa Cibernética
+Development & Coding for Security
 
+Prof. Ms. Fábio H. Cabrini
+Atividade: Check Point 4
+Alunos
+Victor Dias Gonçalves - RM88582
+Filipe Grahl - RM86663
+'''
+
+# Modules:
+import random
+import math
+
+# criar 2 numero primos
 def gerarNP():
     primos = []
-    for numero in range(2, 600):
+    for numero in range(10000, 50000):
         for auxiliar in range(2, numero):
             if numero % auxiliar == 0:
                 break
         else:
             primos.append(numero)
     return random.choice(primos), random.choice(primos)
-q , p = gerarNP()
-print(q,p)
+# Totiente de euler
+def phi(p, q): 
+    return (p - 1) * (q - 1)
+
+
+
